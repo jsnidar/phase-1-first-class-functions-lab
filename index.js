5754145664303ebb71d73b2952caf7b854ab0a2b
Jeremiah <jsnidar@gmail.com>
2021-07-20 17:30:51 -0500

    commit tree db6e4b8f244354b39f3e1a9713bc53ad2d252b06

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arrayOfDrivers) {
+    return arrayOfDrivers.slice(0,2)
+}
+
+function returnLastTwoDrivers(array) {
+    const arrOfLastTwo = array.slice(array.length - 2)
+    return arrOfLastTwo
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+        return fare * integer
+    }
+}
+
+// fareDoubler
+//declare a variable with const
+const fareDoubler = createFareMultiplier(2)
+//assign a function returned by createFareMultiplier() to it
+//invoke fareDoubler() in such a way that:
+    //the new fareDoubler function accepts a fare as its lone argument
+    //it doubles that amount
+
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, func) {
+    return func(drivers) 
+    }
